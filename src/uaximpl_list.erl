95a9cdcf71aed89f67f78a2998ff9df356683724
Karol Skocik <karol.skocik@gmail.com>
2013-04-05 15:46:48 +0200

    finish list impl

--- src/uaximpl_list.erl
+++ src/uaximpl_list.erl
@@ -13,19 +13,40 @@ opts() ->
 
 
 args(put) -> [none_tag];
+args(del) -> [none_tag];
 args(_) -> [].
+
 
 get([]) ->
     fun (Pos, List) -> uax_get:try_get(fun lists:nth/2, Pos, List) end.
 
-put([]) ->
-    fun (_Key, _Val, _List) -> erlang:error({todo, {put, list}}) end.
+put([{none_tag, NoneTag}]) ->
+    fun (Idx, Val, List) -> do_put_default(Idx, Val, List, NoneTag) end.
 
 new([]) ->
     fun () -> [] end.
 
-del([]) ->
-    fun () -> throw(todo) end.
+del([{none_tag, NoneTag}]) ->
+    fun (Idx, List) -> do_del_default(Idx, List, NoneTag) end.
 
 typecheck([]) ->
     fun erlang:is_list/1.
+
+
+%%
+
+do_put_default(Idx, Val, List, NoneTag) ->
+    (is_integer(Idx) andalso Idx > 0) orelse erlang:error(badarg),
+    {RemXs, PrevReversedXs} =
+        uax_util:fold_range(
+          fun (_, {[X | Xs], Acc}) -> {Xs, [X | Acc]};
+              (_, {[], Acc}) -> {[], [NoneTag | Acc]}
+          end, {List, []}, 1, Idx),
+    lists:concat([lists:reverse(PrevReversedXs), [Val], RemXs]).
+
+
+do_del_default(Idx, List, NoneTag) ->
+    (is_integer(Idx) andalso Idx > 0) orelse erlang:error(badarg),
+    do_put_default(Idx, NoneTag, List, NoneTag).
+
+
