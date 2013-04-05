a0f8037e1418a789af8845af4a62d90aacefdc13
Karol Skocik <karol.skocik@gmail.com>
2013-04-05 15:40:24 +0200

    dropped behaviour_info for callback with types (and docs)

--- src/uaximpl.erl
+++ src/uaximpl.erl
@@ -1,15 +1,45 @@
 -module(uaximpl).
 
--export([behaviour_info/1]).
+-type operation() :: get | put | new | del | typecheck.
 
-behaviour_info(callbacks) ->
-    [{opts, 0},
-     {args, 1},
-     {new, 1}, 
-     {get, 1}, 
-     {put, 1},
-     {del, 1},
-     {typecheck, 1}];
+-type modifier() :: key | encode | decode | none_tag.
 
-behaviour_info(_) ->
-    undefined.
+-type c_env() :: [{atom(), any()}]. %% as argument to new, get, ..., tuples are in order given by args/1 function
+
+
+%% @doc Specifies, how are the modifiers checked when creating compilation 
+%% environment for particular node representing layer.
+-callback opts() ->
+    {Required :: [{modifier(), Validator :: fun((any()) -> boolean())}],
+     Supplied :: [{modifier(), Validator :: fun((any()) -> boolean()), DefaultVal :: any()}],
+     Optional :: [{modifier(), Validator :: fun((any()) -> boolean())}]}.
+
+
+%% @doc Specifies, what modifiers are fetched from full node environment 
+%% for compilation of a particular operation.
+-callback args(operation()) -> [modifier()].
+
+                                    
+%% @doc Selects best function according to compilation environment,
+%% which creates empty box for a type implemented by backing module.
+-callback new(c_env()) -> fun (() -> EmptyBox :: any()).
+     
+
+%% @doc Selects best function according to compilation environment,
+%% which fetches a value for a given key from the box.
+-callback get(c_env()) -> fun ((Key :: any(), Box :: any()) -> Value :: any()).
+     
+
+%% @doc Selects best function according to compilation environment,
+%% which associates a given key with a value and returns updated box.
+-callback put(c_env()) -> fun ((Key :: any(), Val :: any(), Box :: any()) -> NewBox :: any()).
+     
+
+%% @doc Selects best function according to compilation environment,
+%% which removes a key (when present) from a box, returning updated box.
+-callback del(c_env()) -> fun ((Key :: any(), Box :: any()) -> NewBox :: any()).
+    
+
+%% @doc Selects best function according to compilation environment,
+%% which removes a key (when present) from a box, returning updated box.
+-callback typecheck(c_env()) -> fun ((Box :: any()) -> boolean()).
