5d8ce5966ae21fc09caac0abff79a96fc5fc8f4d
Karol Skocik <karol.skocik@gmail.com>
2013-04-05 15:46:21 +0200

    finish tuple impl

--- src/uaximpl_tuple.erl
+++ src/uaximpl_tuple.erl
@@ -12,6 +12,7 @@ opts() ->
       {decode, fun (F) -> is_function(F, 1) end}]}.
 
 
+args(put) -> [none_tag];
 args(del) -> [none_tag];
 args(_) -> [].
 
@@ -19,20 +20,56 @@ args(_) -> [].
 get([]) ->
     fun (Pos, Tuple) -> uax_get:try_get(fun erlang:element/2, Pos, Tuple) end.
 
-put([]) ->
-    fun (_Key, _Val, _Tuple) ->
-            erlang:error({todo, {put, tuple}})
-    end.
+put([{none_tag, NoneTag}]) ->
+    fun (Idx, Val, Tuple) -> do_put_default(Idx, Val, Tuple, NoneTag) end.
 
 new([]) ->
     fun () -> {} end.
 
-del([{none_tag, _NoneTag}]) ->
-    fun () -> throw(todo) end.
+del([{none_tag, NoneTag}]) ->
+    fun (Idx, Tuple) -> do_del_default(Idx, Tuple, NoneTag) end.
 
 typecheck([]) ->
     fun erlang:is_tuple/1.
 
 
-%% put_tuple_default(_Idx, _Val, _Tuple) ->
-%%     erlang:error({todo, put_tuple_default}).
+%%
+
+do_put_default(Idx, Val, Tuple, NoneTag) ->
+    (is_integer(Idx) andalso Idx > 0) orelse erlang:error(badarg),
+    if Idx =< size(Tuple) ->
+            setelement(Idx, Tuple, Val);
+       true ->
+            Vals = foldr(fun (KV, Acc) -> [KV | Acc] end, [], Tuple),
+            erlang:make_tuple(Idx, NoneTag, [{Idx, Val} | Vals])
+    end.
+
+
+do_del_default(Idx, Tuple, NoneTag) ->
+    (is_integer(Idx) andalso Idx > 0) orelse erlang:error(badarg),
+    if Idx =< size(Tuple) ->
+            setelement(Idx, Tuple, NoneTag);
+       true ->
+            Tuple
+    end.
+
+
+foldl(Fun, Acc, Tuple) ->
+    Size = size(Tuple),
+    foldl(Fun, Acc, Tuple, 1, Size).
+
+foldl(Fun, Acc, Tuple, Idx, Size) when Idx =< Size ->
+    Acc1 = Fun({Idx, element(Idx, Tuple)}, Acc),
+    foldl(Fun, Acc1, Tuple, Idx + 1, Size);
+foldl(_Fun, Acc, _Tuple, Idx, Size) when Idx > Size ->
+    Acc.
+
+
+foldr(Fun, Acc, Tuple) ->
+    foldr(Fun, Acc, Tuple, size(Tuple)).
+
+foldr(Fun, Acc, Tuple, Idx) when Idx > 0 ->
+    Acc1 = Fun({Idx, element(Idx, Tuple)}, Acc),
+    foldr(Fun, Acc1, Tuple, Idx - 1);
+foldr(_Fun, Acc, _Tuple, Idx) when Idx < 1 ->
+    Acc.
