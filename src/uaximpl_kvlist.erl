441342ba219a3f923d156c842d6afbe76b1aa581
Karol Skocik <karol.skocik@gmail.com>
2013-04-07 19:56:00 +0200

    kvlist impl improvements

--- src/uaximpl_kvlist.erl
+++ src/uaximpl_kvlist.erl
@@ -6,41 +6,55 @@
 
 opts() ->
     {[],
-     [{keypos, fun (X) -> is_integer(X) andalso X > 0 end, 1}, 
-      {valpos, fun (X) -> is_integer(X) andalso X > 0 end, 2},
-      {none_tag, fun (_) -> true end, undefined}],
+     [{keypos, fun (X) -> is_integer(X) andalso X > 0 end, 1},
+      {fill_tag, fun (_) -> true end, undefined}],
      [{key, fun (F) -> is_function(F, 1) end},
+      {valpos, fun (X) -> is_integer(X) andalso X > 0 end},
+      {none_tag, fun (_) -> true end},
       {encode, fun (F) -> is_function(F, 1) end},
       {decode, fun (F) -> is_function(F, 1) end}]}.
 
 
 args(get) -> [keypos, valpos];
-args(put) -> [keypos, valpos, none_tag];
+args(put) -> [keypos, valpos, fill_tag];
 args(del) -> [keypos];
 args(_) -> [].
 
-     
+
+get([{keypos, Keypos}]) ->
+    fun (Key, KVlist) ->
+            case lists:keyfind(Key, Keypos, KVlist) of
+                false -> erlang:error({not_found, Key});
+                Tuple -> Tuple
+            end
+    end;
 get([{keypos, Keypos}, {valpos, Valpos}]) ->
-    fun (Key, Proplist) ->
-            case lists:keyfind(Key, Keypos, Proplist) of
+    fun (Key, KVlist) ->
+            case lists:keyfind(Key, Keypos, KVlist) of
                 false -> erlang:error({not_found, Key});
                 Tuple -> element(Valpos, Tuple)
             end
     end.
 
-put([{keypos, 1}, {valpos, 2} | _]) ->
-    fun (Key, Val, Proplist) -> [{Key, Val} | Proplist] end;
-put([{keypos, Keypos}, {valpos, Valpos}, {none_tag, NoneTag}]) ->
+
+put([{keypos, _Keypos}, {fill_tag, _FillTag}]) ->
+    fun (_Key, Val, KVlist) -> [Val | KVlist] end;
+put([{keypos, 1}, {valpos, 2}, {fill_tag, _FillTag}]) ->
+    fun (Key, Val, KVlist) -> [{Key, Val} | KVlist] end;
+put([{keypos, Keypos}, {valpos, Valpos}, {fill_tag, FillTag}]) ->
     Size = max(Keypos, Valpos),
-    fun (Key, Val, Proplist) ->
-            [erlang:make_tuple(Size, NoneTag, [{Keypos, Key}, {Valpos, Val}]) | Proplist]
+    fun (Key, Val, KVlist) ->
+            [erlang:make_tuple(Size, FillTag, [{Keypos, Key}, {Valpos, Val}]) | KVlist]
     end.
+
 
 new([]) ->
     fun () -> [] end.
 
+
 del([{keypos, Keypos}]) ->
-    fun (Key, Proplist) -> lists:keydelete(Key, Keypos, Proplist) end.
+    fun (Key, KVlist) -> lists:keydelete(Key, Keypos, KVlist) end.
+
 
 typecheck([]) ->
     fun erlang:is_list/1.
